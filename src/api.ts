654c67b78608e8dcd232ee4e7b39ac59d69d9744
ktekosi <victor.olaru@gmail.com>
2024-03-27 23:10:24 +0200

    Fixed linting.

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { type ApiFunction } from './server.ts';
 import { z } from 'zod';
 import { type Document, MongoClient } from 'mongodb';
-import {CacheManager, type CacheOptions, ChangeStreamManager, ChangeStreamsStats, type LiveCache} from './cache.ts';
+import { CacheManager, type CacheOptions, ChangeStreamManager, type ChangeStreamsStats, type LiveCache } from './cache.ts';
 import { denormalize, normalize } from './converter.ts';
 
 const CountParamsSchema = z.object({
